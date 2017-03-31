3b0c9a57236a9f7ae535b3dcf1c8d07b13031f5f
Carlos <up201504749@fe.up.pt>
2017-03-31 18:53:31 +0100

    updated sfind

--- sfind.c
+++ sfind.c
@@ -5,27 +5,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/times.h>
+#include <sys/stat.h>
+
+ int print = 0, delete = 0;
+ char* name_file;
+ char file_type;
+ int perm_type;
+ char initial_path;
 
 
-int main(int argc, char *argv[] ){
-    char* name_file;
-    char file_type;
-    int perm_type;
-    int print = 0, delete = 0;
-    
-    return 0;
-}
+ int verifyArgs(int argc, char *argv[]){
+    int i = 2;
 
-int verifyArgs(int argc, char *argv[]){
-    int i = 1;
-    for(; i < argc; i++)
+    initial_path = *argv[1];
+    if(initial_path == '.' || initial_path == '/' || initial_path == '~')
     {
+        
+         for(; i < argc; i++)
+     {
         if(*argv[i] == "-name"){
+            if(*argv[i+1] == " ")
+                return -1;
+            else 
             name_file = argv[++i];
         }
         else if(*argv[i] == "-type"){
             if(*argv[i+1] != 'd' && *argv[i+1] != 'l' && *argv[i+1] != 'f')
-                break;
+            {
+                return -1;
+            
+            }   
             else
             file_type = *argv[++i];
         }
@@ -33,10 +42,74 @@ int verifyArgs(int argc, char *argv[]){
             perm_type = *argv[++i];
         }
         else if(*argv[i] == "-print"){
-           //perm_typer
+                print = 1;
         }
         else if(*argv[i] == "-delete"){
-           //perm_type = *argv[++i];
-        }
+           delete = 1;
+        }else return -1;
     }
+    return 0;
+    }else return -1;
+   
 }
+
+char* getInitialPath(char *envp[])
+{
+    char user[4];
+ strcpy(user,"PWD");
+   
+  char** env = envp;
+  char* substring;
+	
+	for(env = envp ; *env != 0;env++)
+	{
+		
+		
+		substring =strtok(*env,"=");
+		if(strcmp(user,substring) == 0)
+		{
+			substring= strtok(NULL,"\0");
+			printf("%s\n",substring);
+		}
+		
+		
+	}
+    return substring;
+}
+
+char* search(struct stat buf)
+{
+    
+    lstat(pwd,&buf);
+    if(S_ISDIR(buf.st_mode))
+   {
+
+    
+    
+   }else if(S_ISREG(buf.st_mode))
+   {
+
+   }else if(S_ISLNK(buf.st_mode))
+   {
+
+   }
+}
+
+int main(int argc, char *argv[],char *envp[]){
+    
+   if(verifyArgs(argc,argv) == -1)
+   {
+       return -1;
+   }
+   char* pwd = getInitialPath(envp);
+   struct stat buf;
+   
+
+   
+    return 0;
+}
+
+
+
+
+

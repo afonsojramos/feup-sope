5a9d5c7a71ac9875acfc6da184fbd1202fd319b8
Diogo Filipe Alves Dores <up201504614@B01AT0709.fe.up.pt>
2017-05-05 18:57:30 +0100

    Sauna fifo entrance + tentativa de ler uma fifo

--- Trabalho2/sauna.c
+++ Trabalho2/sauna.c
@@ -14,15 +14,64 @@
 
 
 int n_lugares;
+int readline(int fd, char *str);
 
-int checkParameters(int argc,char[] *argv)
-{
+int checkParameters(int argc, char[] *argv){
+
     if(argc != 2)
     return -1;
     n_lugares = argv[1];
     return 0;
 }
-int main(int argc, char[] *argv)
+
+int createFifoEntrance()
 {
-    
+    if(mkfifo("/tmp/rejeitados",0660) < 0){
+       if (errno == EEXIST){
+          printf("FIFO 'tmp/rejeitados' already exists\n!");
+          return -1;
+       } else {
+           printf("Can't create FIFO\n");
+           return -1;
+       }
+    } else
+        return 0;
+
+}
+
+int readline(int fd, char *str) {
+
+    int n;
+    char gender;
+
+    do {
+      n = read(fd,str,1);
+    } while (n>0 && *str++ != '\n');
+
+    return (n>0);
+}
+
+int main(int argc, char[] *argv){
+    if(argc != 2)
+
+  int fd;
+  char str[MAX_MSG_LEN];
+
+  if(checkParameters(argc, argv) != 0){
+    printf("The parameters are wrong.\n");
+    return -1;
+  }
+
+  if(createFifoEntrance() != 0)
+      return -1;
+
+  if(fd = open("/tmp/rejeitados",O_RDONLY)) == -1)
+  {
+      printf("Error opening FIFO\n");
+      return -1;
+  }
+
+  while(readline(fd,str))
+      printf("%s",str);
+
 }
